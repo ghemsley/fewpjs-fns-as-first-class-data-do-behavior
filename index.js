8d0a2f8878e2470e28b5efbff98ccc438d9bf8d6
ghemsley <ghemsley@protonmail.ch>
2021-05-04 15:30:24 -0500

    commit tree 0bece1fe792c79fc0418a02dadf82b350874aaec

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+const greet = (time) => {
+  const numberStrings = time.split(':')
+  const numbers = numberStrings.map((string) => {
+    return parseInt(string)
+  })
+  if (numbers[0] < 12) {
+    return 'Good Morning'
+  } else if (numbers[0] >= 12 && numbers[0] < 17) {
+    return 'Good Afternoon'
+  } else {
+    return 'Good Evening'
+  }
+}
 /* Write your implementation of displayMessage() */
+const displayMessage = (string) => {
+  const element = document.getElementById('greeting')
+  element.innerText = string
+}
